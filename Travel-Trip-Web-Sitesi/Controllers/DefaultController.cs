536577968b5efab5b26c3a577c758d31a1ac575f
sema <72384075+semakirdar@users.noreply.github.com>
2021-03-07 15:25:01 +0300

    C# MVC webSite

---
+++ Travel-Trip-Web-Sitesi/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Travel_Trip_Web_Sitesi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Travel-Trip-Web-Sitesi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Travel_Trip_Web_Sitesi.MvcApplication" Language="C#" %>
