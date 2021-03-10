28e9daa7f64ccc6fe7022223c84e7c9c53f88584
sema <72384075+semakirdar@users.noreply.github.com>
2021-03-10 19:27:58 +0300

    C# mvc travel web site

--- Travel-Trip-Web-Sitesi/Controllers/DefaultController.cs
+++ Travel-Trip-Web-Sitesi/Controllers/DefaultController.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Travel_Trip_Web_Sitesi.Models.Siniflar;
 
 namespace Travel_Trip_Web_Sitesi.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
         }
         public ActionResult About()
         {
             return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(X => X.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial2()
+        {
+            var deger = c.Blogs.Where(x => x.ID == 1).ToList();
+            return PartialView(deger);
         }
     }
 }
